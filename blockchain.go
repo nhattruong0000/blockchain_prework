04b93dd0da0815292ac1d6a24b9c52765d6f953c
TruongBN2 <truongbn2@fsoft.com.vn>
2018-02-12 15:56:51 +0700

    first commit

--- blockchain.go
+++ blockchain.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/davecgh/go-spew/spew"
 )
@@ -22,22 +23,47 @@ type Block struct {
 
 // InitBlockchain creates our first Genesis node.
 func InitBlockchain() {
-	fmt.Println("******TODO: IMPLEMENT InitBlockchain!******")
+	// fmt.Println("******TODO: IMPLEMENT InitBlockchain!******")
 	spew.Dump(Blockchain)
 	// Fill me in, noble warrior.
+	block := Block{"Genesis Block", time.Now().Unix(), []byte{}, []byte{}}
+	block.Hash = block.calculateHash()
+	Blockchain = append(Blockchain, block)
 }
 
 // NewBlock creates a new Blockchain Block.
 func NewBlock(oldBlock Block, data string) Block {
-	fmt.Println("******TODO: IMPLEMENT NewBlock!******")
-	return Block{}
+	// fmt.Println("******TODO: IMPLEMENT NewBlock!******")
+	block := Block{Data: data, Timestamp: time.Now().Unix(), PrevHash: oldBlock.Hash, Hash: []byte{}}
+	block.Hash = block.calculateHash()
+	// fmt.Println("data: " + block.Data)
+	// fmt.Printf("timestamp: %d", block.Timestamp)
+	// fmt.Println()
+	// fmt.Printf("preHash: %x", block.PrevHash)
+	// fmt.Println()
+	// fmt.Printf("currentHash: %x", block.Hash)
+	// fmt.Println()
+	// fmt.Println("******TODO: END NewBlock!******")
+	// fmt.Println()
+	// fmt.Println()
+	// fmt.Println()
+	return block
 }
 
 // AddBlock adds a new block to the Blockchain.
 func AddBlock(b Block) error {
-	fmt.Println("******TODO: IMPLEMENT AddBlock!******")
+	// fmt.Println("******TODO: IMPLEMENT AddBlock!******")
 	spew.Dump(Blockchain)
 	// Fill me in, brave wizard.
+	prevBlock := Blockchain[len(Blockchain)-1]
+	// fmt.Println(prevBlock)
+	// fmt.Println(b)
+	if bytes.Compare(b.PrevHash, prevBlock.Hash) != 0 {
+		// return errors.New("Error block")
+		return fmt.Errorf("New Block should have Hash: %x, but has Hash: %x",
+			prevBlock.Hash, b.PrevHash)
+	}
+	Blockchain = append(Blockchain, b)
 	return nil
 }
 
